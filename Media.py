162af2fccfc32236589266c1bace43cd54f3b4a6
julyanekarinadosantos <julyane.santos500@gmail.com>
2025-05-13 08:48:29 -0300

    Primeiro Commit

---
+++ Media.py
@@ -0,0 +1,8 @@
+nome = input('qual é o nome do aluno? ')
+
+n1 = int (input('primeira nota: '))
+n2 = int (input('segunda nota: '))
+n3 = int (input('terceira nota: '))
+media = (n1+n2+n3) /3
+print(media)
+print('aluno' , nome , 'tirou' , media)
---
+++ app.py
@@ -0,0 +1,12 @@
+
+
+nome = input ('qual é o seu nome?')
+idade = input('qual é a sua idade?')
+cor = input(' qual sua cor favorita?')
+fruta = input(' qual sua fruta favorita?')
+hobby = input('qual seu hobby?')
+animal = input('qual seu animal favorito?')
+musica = input('qual sua musica favorita?')
+let = input('voce ama muito a leticia?')
+
+print('NOME:', nome ,'IDADE', idade ,'COR', cor , 'FRUTA', hobby , 'ANIMAL' , animal , 'MUSICA', musica , 'LET', let)
